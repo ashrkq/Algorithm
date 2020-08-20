712f31fe311d0b95108f186605e5f868609ff409
ashrkq <ahrkq@gmil.com>
2020-08-20 23:58:10 +0800

    2020-08-20-end

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,20 +1,26 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<map>
+#include<cstring>
 using namespace std;
-int id;
 map<string, int>m;
-int pre[100010];
+int *pre;
+// 查找祖先然后路径压缩
 int find(int k){
-    return pre[k] = pre[k]==k?k:find(pre[k]);
+    return pre[k] = (pre[k]==k?k:find(pre[k]));
 }
+// 判断两个人是否属于同一祖先
 void merge(int x, int y){
     int xx = find(x), yy = find(y);
     if(xx != yy )pre[xx] = yy;
 }
+// 初始化设置 每个人的祖先都是自己
 void init(int n){
+    pre = (int*)malloc(sizeof(int)*(n+1));
     for(int i=1;i<=n;i++) pre[i] = i;
 }
 int main(){
     int n; cin>>n;
+    int id = 0;
     string s1, s2; init(n);
     for(int i=0;i<n;i++){
         int op; cin>>op>>s1>>s2;
@@ -26,6 +32,8 @@ int main(){
         }
         else{
             int k1 = m[s1], k2 = m[s2];
+            // 如果不存在就直接表示不在同一组织内
+            if(k1==0 || k2 == 0) {puts("no"); continue;}
             if(find(k1) == find(k2))
                 puts("yes");
             else
