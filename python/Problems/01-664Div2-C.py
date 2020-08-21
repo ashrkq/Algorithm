862e5e4de6482bef6c23685626145785e287cc87
ashrkq <ahrkq@gmil.com>
2020-08-21 19:02:57 +0800

    贪心

--- python/Problems/01-664Div2-C.py
+++ python/Problems/01-664Div2-C.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
             for bi in b:
                 if (k|(ai&bi)) == k:
                     flag = True
+                    
                     break
             if not flag: break  # 没有找到满足k这个模式的的字符直接结束
         else: break # 对于所有的i都可以找到满足k这个模式的数字直接结束
--- python/main.py
+++ python/main.py
@@ -1,16 +1,19 @@
-# 异或的性质
 if __name__ == "__main__":
     data = [*open(0)]
     dt = lambda x:map(int,data[x].split())
+    n,m = *dt(0),
     a = *dt(1),
     b = *dt(2),
-    for k in range(512):
-        for ai in a:
-            flag = False
+    ans = 0
+    c = [0 for i in range(n)]
+    for k in range(n):
+        mint = (0,0)
+        for i,ai in enumerate(a):
+            if c[i]: continue
+            maxx = ai|ans
             for bi in b:
-                if (k|(ai&bi)) == k:
-                    flag = True
-                    break
-            if not flag: break  # 没有找到满足k这个模式的的字符直接结束
-        else: break # 对于所有的i都可以找到满足k这个模式的数字直接结束
-    print(k)
+                maxx = min(maxx, ai&(bi|ans))
+            mint = mint if mint[1]>maxx else (i,maxx)
+        ans = mint[1]|ans
+        c[mint[0]] = 1
+    print(ans)
