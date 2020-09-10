b86a149d63dee38670b876c4a689cee7680c1f23
ashrkq <ahrkq@gmil.com>
2020-09-10 20:14:26 +0800

    线段树单点更新

---
+++ cpp/Template/34-十大排序算法.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 冒泡排序基本思想
+
+class Ordering
+{
+public:
+    static void bubble_sort(vector<int> &arr)
+    {
+        // 冒泡排序 每次选择一个比较前n-i个元素然后把最大的放到最后面
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n - 1 - i; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                    count++;
+                }
+            // 优化方式 没有变化就说明已经有序了
+            if (count == 0)
+                return;
+        }
+    }
+
+    static void selection_sort(vector<int> &arr)
+    {
+        // 选择排序思想类似冒泡排序 只是不进行每次交换了每次直只知道最小的只交换一次
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            int temp = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[temp] > arr[j])
+                    temp = j;
+            swap(arr[temp], arr[i]);
+        }
+    }
+
+    static void insertion_sort(vector<int> &arr)
+    {
+        // 插入排序每次选择下一个元素其他元素整体后移到一个小于等于当前元素的位置停止后移
+        int n = arr.size();
+        for (int i = 1; i < n; i++)
+        {
+            int t = arr[i], j = i - 1;
+            while (j >= 0 && arr[j] > t)
+                arr[j + 1] = arr[j], j--;
+            arr[j + 1] = t;
+        }
+    }
+
+    static void shell_sort(vector<int> &arr)
+    {
+        // 希尔排序 每次对插入排序的间隔进行限制 每次二分这个间隔 插入排序越是有序越是时间复杂度低
+        // 每次都使序列比原来更有序
+        int n = arr.size();
+        int k = n / 2;
+        while (k)
+        {
+            for (int i = k; i < n; i++)
+            {
+                int j = i, t = arr[i];
+                while (j - k >= 0 && t < arr[j - k])
+                    arr[j] = arr[j - k], j -= k;
+                arr[j] = t;
+            }
+            k /= 2;
+        }
+    }
+
+    static void merge_sort(vector<int> &arr)
+    {
+        // 归并排序 时间复杂对最多是O(n*log(n)) 每次把集合分成两份得到两个有序的序列然后以O(n) 进行合并
+        // 最多分割log(n) 次
+        int n = arr.size();
+        if (n < 2)
+            return;
+        int mid = n / 2;
+        vector<int> arrl(arr.begin(), arr.begin() + mid);
+        vector<int> arrr(arr.begin() + mid, arr.end());
+        merge_sort(arrl);
+        merge_sort(arrr);
+        int l = 0, r = 0, ln = arrl.size(), rn = arrr.size();
+        arr.clear();
+        while (l < ln || r < rn)
+        {
+            if (l < ln && r < rn)
+            {
+                if (arrl[l] <= arrr[r])
+                    arr.push_back(arrl[l]), l++;
+                else
+                    arr.push_back(arrr[r]), r++;
+            }
+            else if (l < ln)
+                arr.push_back(arrl[l]), l++;
+            else
+                arr.push_back(arrr[r]), r++;
+        }
+    }
+};
+template <class T>
+void test_sort(T &ordering, vector<int> &arr, int len)
+{
+    clock_t startTime, endTime;
+    srand((int)time(0));
+    arr.clear();
+    for (int i = 0; i < 20; ++i)
+        arr.push_back(rand() % 100);
+    for (int a : arr)
+        printf("%d ", a);
+    puts("");
+    ordering(arr);
+    for (int a : arr)
+        printf("%d ", a);
+    puts("");
+    arr.clear();
+    for (int i = 0; i < len; ++i)
+        arr.push_back(rand());
+    startTime = clock();
+    ordering(arr);
+    endTime = clock();
+    cout << "The run " << len << "数据 time is: " << (endTime - startTime) / 1000 << "ms" << endl;
+}
+
+int main()
+{
+    Ordering ordering = Ordering();
+    vector<int> arr;
+    printf("-------------冒泡排序--------------\n");
+    test_sort(ordering.bubble_sort, arr, 10000);
+    printf("-------------选择排序--------------\n");
+    test_sort(ordering.selection_sort, arr, 10000);
+    printf("-------------插入排序--------------\n");
+    test_sort(ordering.insertion_sort, arr, 10000);
+    printf("-------------希尔排序--------------\n");
+    test_sort(ordering.shell_sort, arr, 1000000);
+    printf("-------------归并排序--------------\n");
+    test_sort(ordering.merge_sort, arr, 100000);
+
+    return 0;
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,19 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-// 暴力查找加修改区间的最小值
-int num[10010];
+int wit[1000010];
+int tree[4 * 1000010];
+// 线段树 单点更新
+// 把一个序列每次二分 每个节点代表一个区间 每个节点记录了这个区间的最小值
+// 依次递归更新区间的最值
+// 依次向上更新 使用了堆的结构 以数组的方式存储
+void build(int l, int r, int idx){
+    if( l == r){ scanf("%d", tree + idx); return; }
+    int mid = ( l + r ) >> 1;
+    build(l, mid, (idx << 1) + 1);
+    build(mid + 1, r, (idx << 1) + 2);
+    tree[idx] = min(tree[(idx<<1) + 1], tree[(idx<<1) + 2]);
+}
+int query(int l, int r,int idx, int ql, int qr){
+    if(ql <= l && r <= qr) return tree[idx];
+    int mid = (l + r) >> 1;
+    if(qr <= mid ) return query(l, mid, (idx << 1) + 1, ql, qr);
+    if(ql > mid) return query(mid + 1, r, (idx << 1) + 2, ql, qr);
+    return min( query(l, mid, (idx << 1) + 1, ql, mid), query(mid + 1, r, (idx << 1) + 2, mid+1, qr) );
+}
+void update(int l, int r, int idx, int x, int value){
+    if(l == r){ tree[idx] = value; return; }
+    int mid = (l+r) >> 1;
+    if(mid >= x) update(l, mid, (idx << 1) + 1, x, value);
+    else update(mid + 1, r, (idx << 1) + 2, x, value);
+    tree[idx] = min(tree[(idx << 1)+1], tree[(idx << 1) + 2]);
+}
+
+
 int main(){
-    int n; scanf("%d", &n);
-    for(int i=0;i<n;++i) scanf("%d", num+i);
+    int n; scanf("%d",&n);
+    build(0, n-1, 0);
     int m; scanf("%d", &m);
     while(m--){
-        int q, l, r; scanf("%d %d %d", &q, &l, &r);
-        if(q) num[l-1] = r;
-        else{
-            int minn = num[l-1];
-            for(int i = l-1; i<r;++i) minn = min(minn,num[i]);
-            printf("%d\n", minn);
-        }
+        int q, x, y; scanf("%d %d %d", &q, &x, &y);
+        if(q) update(0, n-1, 0, x-1, y);
+        else printf("%d\n", query(0, n-1, 0, x-1, y-1));
     }
     return 0;
 }
